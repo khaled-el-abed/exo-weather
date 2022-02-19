fdf4d1e15312dbc78b1de99cc03cef59dc79e0bb
El Abed <khaled.el.abed@hotmail.com>
2022-02-19 16:03:50 +0100

    add weather fetcher

---
+++ ExoWeather/Pods/Target Support Files/Pods-ExoWeather-ExoWeatherUITests/Pods-ExoWeather-ExoWeatherUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ExoWeather_ExoWeatherUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ExoWeather_ExoWeatherUITests
+@end
---
+++ ExoWeather/Pods/Target Support Files/Pods-ExoWeatherTests/Pods-ExoWeatherTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ExoWeatherTests : NSObject
+@end
+@implementation PodsDummy_Pods_ExoWeatherTests
+@end
